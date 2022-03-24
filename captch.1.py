8276771b222c11408d01fe5fb0f318360cc802e1
10746025 <dmc950213@gmail.com>
2022-03-24 15:32:55 +0800

    去除雜亂檔案

--- captch.1.py
+++ captch.1.py
@@ -11,21 +11,21 @@ for i in range(len(inv)):
     for j in range(len(inv[i])):
         if inv[i][j] ==255:
             count=0
-            for k in range(-2,3):#設定範圍檢查，判斷是否雜點
-                for l in range(-2,3):
+            for k in range(-4,3):#設定範圍檢查，判斷是否雜點
+                for l in range(-4,3):
                     try:
                         if inv[i+k][j+1]==255:
                             count+=1
                     except IndexError:
                         pass
-            if count <=4: #周圍小於六個白點則將白點去除
-                inv[i][j]=0
+            if count <=7: #周圍小於七個白點則將白點去除
+                inv[i][j]=0                
 dilation=cv2.dilate(inv,(8,8),iterations=1)  #圖形加粗
 cv2.imwrite("media\\bank_result.jpg",dilation)  #存檔
 
-child = subprocess.Popen('tesseract media\\bank_result.jpg result')  #OCR辨識
+child = subprocess.Popen('tesseract media\\bank_result.jpg media\\result')  #OCR辨識
 child.wait()
-text = open('result.txt').read().strip()
+text = open('media\\result.txt').read().strip()
 print("驗證碼為 " + text)
                     
             
